03ceb1adfab26d9dc881b22a916055393c7e843f
Mariusz Derda <mariusz.derda@gmail.com>
2021-07-03 17:37:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <router-link to="/">Spacer</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped />
