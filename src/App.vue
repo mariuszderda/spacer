4d9bb9713f70f9b1caf310c1813a215b00e0f9ae
Mariusz Derda <mariusz.derda@gmail.com>
2021-07-03 20:10:06 +0200

    Add claim component and remove About component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div class="app">
     <div class="nav">
       <router-link to="/">Spacer</router-link>
-      <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
@@ -12,6 +11,13 @@
 </script>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap');
 
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Montserrat, sans-serif;
+}
 </style>
